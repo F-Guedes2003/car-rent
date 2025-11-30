8ac979a18b9601810075f16accd10bdb148c2ab7
Aislan <aislan.pepi@outlook.com>
2025-11-30 14:43:26 -0300

    test: integration test return customer updated

--- src/test/java/br/ifsp/vvts/controller/CustomerControllerTest.java
+++ src/test/java/br/ifsp/vvts/controller/CustomerControllerTest.java
@@ -3,6 +3,7 @@ package br.ifsp.vvts.controller;
 import br.ifsp.vvts.domain.dto.CreateCarRequest;
 import br.ifsp.vvts.domain.dto.CreateCustomerRequest;
 import br.ifsp.vvts.domain.dto.UpdateCarRequest;
+import br.ifsp.vvts.domain.dto.UpdateCustomerRequest;
 import br.ifsp.vvts.infra.persistence.repository.CarRepository;
 import br.ifsp.vvts.infra.persistence.repository.CustomerRepository;
 import br.ifsp.vvts.security.user.User;
@@ -50,6 +51,36 @@ class CustomerControllerTest extends BaseApiIntegrationTest {
                 .log().ifValidationFails(LogDetail.BODY)
                 .statusCode(201)
                 .body("name", equalTo("Aislan"))
+                .body("cpf", equalTo("514.302.036-09"));
+    }
+
+    @Tag("ApiTest")
+    @Tag("IntegrationTest")
+    @Test
+    @DisplayName("Should update a car and return 200 with updated values")
+    void shouldUpdateCarAndReturn200WithCarObjectAsPayload() {
+
+        var request = new CreateCustomerRequest("Aislan","51430203609");
+
+        given()
+                .contentType("application/json")
+                .port(port)
+                .header("Authorization", "Bearer " + token)
+                .body(request)
+                .post("/api/v1/customers");
+
+        var updateRequest = new UpdateCustomerRequest("Aislan Pepi");
+
+        given()
+                .contentType("application/json")
+                .port(port)
+                .header("Authorization", "Bearer " + token)
+                .body(updateRequest)
+                .put("/api/v1/customers/" + request.cpf())
+                .then()
+                .log().ifValidationFails(LogDetail.BODY)
+                .statusCode(200)
+                .body("name", equalTo("Aislan Pepi"))
                 .body("cpf", equalTo("514.302.036-09"));
     }
 }
