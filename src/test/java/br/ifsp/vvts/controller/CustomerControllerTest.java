274f340592d82d6e447aa2312e643247154921af
Aislan <aislan.pepi@outlook.com>
2025-11-30 14:36:47 -0300

    test: integration test return customer created

---
+++ src/test/java/br/ifsp/vvts/controller/CustomerControllerTest.java
@@ -0,0 +1,55 @@
+package br.ifsp.vvts.controller;
+
+import br.ifsp.vvts.domain.dto.CreateCarRequest;
+import br.ifsp.vvts.domain.dto.CreateCustomerRequest;
+import br.ifsp.vvts.domain.dto.UpdateCarRequest;
+import br.ifsp.vvts.infra.persistence.repository.CarRepository;
+import br.ifsp.vvts.infra.persistence.repository.CustomerRepository;
+import br.ifsp.vvts.security.user.User;
+import io.restassured.filter.log.LogDetail;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+class CustomerControllerTest extends BaseApiIntegrationTest {
+
+    String token;
+    User user;
+
+    @Autowired
+    CustomerRepository repository;
+
+    @AfterEach
+    void tearDown() {
+        repository.deleteAll();
+    }
+
+    @BeforeEach
+    void setup() {
+        user = register("123password");
+        token = authenticate(user.getEmail(), "123password");
+    }
+
+    @Tag("ApiTest")
+    @Tag("IntegrationTest")
+    @Test
+    @DisplayName("Should register a customer and return 201 with customer object as payload")
+    void shouldRegisterCustomerAndReturn201WithCustomerObjectAsPayload() {
+
+        var request = new CreateCustomerRequest("Aislan","51430203609");
+
+        given()
+                .contentType("application/json")
+                .port(port)
+                .header("Authorization", "Bearer " + token)
+                .body(request)
+                .when().post("/api/v1/customers")
+                .then()
+                .log().ifValidationFails(LogDetail.BODY)
+                .statusCode(201)
+                .body("name", equalTo("Aislan"))
+                .body("cpf", equalTo("514.302.036-09"));
+    }
+}
